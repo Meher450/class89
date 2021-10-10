f97bce16c3f687fca5aafca944efa4eeacbea4cf
Meher <82945138+Meher450@users.noreply.github.com>
2021-10-10 16:13:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCdObv7Wx-5ITgekotX55Ms33M8XIbYG70",
+  authDomain: "story-3ed64.firebaseapp.com",
+  databaseURL: "https://story-3ed64-default-rtdb.firebaseio.com",
+  projectId: "story-3ed64",
+  storageBucket: "story-3ed64.appspot.com",
+  messagingSenderId: "373176937512",
+  appId: "1:373176937512:web:d77dcc1e11a8e79ef2191d"
+};
